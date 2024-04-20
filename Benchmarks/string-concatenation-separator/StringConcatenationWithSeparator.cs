762a5d7c722f0cdd0a36bbd9ce59f6ae0b41eb5d
Andrea Carratta <github@devandreacarratta.it>
2024-04-20 17:06:42 +0200

    StringConcatenation & StringConcatenationSeparator

---
+++ Benchmarks/string-concatenation-separator/StringConcatenationWithSeparator.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace BenchmarksDotnet.Benchmarks
+{
+    public class StringConcatenationWithSeparator
+    {
+        [Params(10, 100, 1000, 10000)]
+        public int NumberOfItems;
+
+        private const string SEPARATOR = "|";
+
+        private List<string> _items = new();
+
+        [IterationSetup()]
+        public void Setup()
+        {
+            _items = new List<string>();
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                _items.Add(i.ToString("D5"));
+            }
+        }
+
+        [Benchmark]
+        public void StringBuilderInterpolation()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string item in _items)
+            {
+                stringBuilder.Append($"{item}{SEPARATOR}");
+            }
+            _ = stringBuilder.ToString();
+        }
+
+        [Benchmark]
+        public void StringBuilderFormat()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string item in _items)
+            {
+                stringBuilder.AppendFormat("{0}{1}", item, SEPARATOR);
+            }
+            _ = stringBuilder.ToString();
+        }
+
+        [Benchmark]
+        public void StringJoinToString()
+        {
+            _ = string.Join(SEPARATOR, _items);
+        }
+
+        [Benchmark]
+        public void LinqAggregate()
+        {
+            _ = _items.Aggregate((current, next) => current + SEPARATOR + next);
+        }
+
+        [Benchmark]
+        public void LinqAggregateInterpolation()
+        {
+            _ = _items.Aggregate((current, next) => $"{current}{SEPARATOR}{next}");
+        }
+    }
+}
