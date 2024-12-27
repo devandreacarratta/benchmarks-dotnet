e11f508569c19f80ec916cae1a1e7633330372f5
Andrea Carratta <46504271+devandreacarratta@users.noreply.github.com>
2024-12-27 13:33:29 +0100

    list-count-greater-than-zero-vs-any (#6)

---
+++ Benchmarks/list-count-greater-than-zero-vs-any/ListCountGreaterThanZeroVsAny.cs
@@ -0,0 +1,32 @@
+﻿namespace BenchmarksDotnet.Benchmarks;
+
+public class ListCountGreaterThanZeroVsAny
+{
+    [Params(1000, 10_000, 100_000)]
+    public int ItemsToAdd;
+
+    private List<Guid> _list = new();
+
+    [IterationSetup()]
+    public void Setup()
+    {
+        _list = new();
+
+        for (int i = 0; i < ItemsToAdd; i++)
+        {
+            _list.Add(Guid.NewGuid());
+        }
+    }
+
+    [Benchmark]
+    public void ListCountGreaterThanZero()
+    {
+        _=_list.Count > 0;
+    }
+
+    [Benchmark]
+    public void ListAny()
+    {
+        _ = _list.Any();
+    }
+}
