d9ca7c131cfef392f2210697cc7062a1938aae44
Andrea Carratta <github@devandreacarratta.it>
2024-12-27 14:18:46 +0100

    string-start-with

---
+++ Benchmarks/string-start-with/StringStartWith.cs
@@ -0,0 +1,23 @@
+﻿namespace BenchmarksDotnet.Benchmarks;
+
+public class StringStartWith
+{    
+    public const string FIXED_STRING_VALUE = "hello world";
+ 
+    public string StringItem { get; set; } = "h";
+
+    public char CharItem { get; set; } = 'h';   
+
+    [Benchmark]
+    public void StartWithString()
+    {
+        _ = FIXED_STRING_VALUE.StartsWith(StringItem);
+    }
+
+    [Benchmark]
+    public void StartWithChar()
+    {
+        _ = FIXED_STRING_VALUE.StartsWith(CharItem);
+    }
+}
+
